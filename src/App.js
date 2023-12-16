9f73ffacbdf058bd8a7a0fda0004c94a6cb44b61
nishu1103 <nishantk1103@gmail.com>
2023-12-16 20:46:36 +0530

    mm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import NAPS from './pages/services/NAPS/NAPS';
+// import NAPS from './pages/Services/NAPS/NAPS';
 import NATS from './pages/services/NATS/NATS';
-// import About from './pages/about/about';
-// import Apprenticeship from './pages/services/apprenticeship/apprenticeship';
-// import Flexi from './pages/services/flexi-staffing/flexi';
-// import Manpower from './pages/services/manpower-outsourcing/manpower';
+import About from './pages/about/about';
+import Apprenticeship from './pages/services/apprenticeship/apprenticeship';
+import Flexi from './pages/services/flexi-staffing/flexi';
+import Manpower from './pages/services/manpower-outsourcing/manpower';
 import Permanent from './pages/services/permanent-staffing/permanent';
-// import Staffing from './pages/services/staffing/staffing';
+import Staffing from './pages/services/staffing/staffing';
 
 function App() {
   return (
     <div className="App">
-      {/* <Apprenticeship/> */}
-      {/* <Staffing/> */}
-      {/* <Permanent/> */}
-      {/* <Flexi/> */}
-      {/* <Manpower/> */}
-      {/* <NAPS/> */}
+      <Apprenticeship/> 
+      <Staffing/> 
+      <Permanent/> 
+      <Flexi/>
+       <Manpower/>
+       {/* <NAPS/> */}
       <NATS/>
     </div>
   );
